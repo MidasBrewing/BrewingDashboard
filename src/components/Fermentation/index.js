e62f87d43881617e10d61876b3db404b0babb210
Mikael Wikblom <mikael.wikblom@trustly.com>
2019-08-24 09:16:05 +0200

    fixed bubble counter

--- src/components/Fermentation/index.js
+++ src/components/Fermentation/index.js
@@ -1,274 +1,286 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React, { Component } from "react";
+import _ from "lodash";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import WarningIcon from '@material-ui/icons/Warning';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import WarningIcon from "@material-ui/icons/Warning";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
-import { withFirebase } from '../Firebase';
+import { withFirebase } from "../Firebase";
 
 am4core.useTheme(am4themes_animated);
 
 const styles = theme => ({
-    card: {
-        minWidth: 275,
-    },
-    title: {
-        fontSize: 20,
-    },
-    icon: {
-        marginLeft: theme.spacing.unit * 2,
-        verticalAlign: 'bottom'
-    },    
+  card: {
+    minWidth: 275
+  },
+  title: {
+    fontSize: 20
+  },
+  icon: {
+    marginLeft: theme.spacing.unit * 2,
+    verticalAlign: "bottom"
+  }
 });
 
 class Fermentation extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: false,
-            lastTimeSpanInSec: 0,
-            lastBubbleAt: null
-        };
+    this.state = {
+      loading: false,
+      lastTimeSpanInSec: 0,
+      lastBubbleAt: null
+    };
 
-        this.id = _.uniqueId("chart-");
+    this.id = _.uniqueId("chart-");
 
-        this.doResetFermentationEntires = this.doResetFermentationEntires.bind(this);
-        this.loadFermentations = this.loadFermentations.bind(this);
-    }
+    this.doResetBubbleEntires = this.doResetBubbleEntires.bind(this);
+    this.loadBubbles = this.loadBubbles.bind(this);
+  }
 
-    loadFermentations() {
-        const { batch, firebase } = this.props;
+  loadBubbles() {
+    const { batch, firebase } = this.props;
 
-        this.setState({ loading: true });
+    this.setState({ loading: true });
 
-        firebase.fermentations(batch)
-            .orderByKey()
-            .limitToLast(20)
-            .on('value', snapshot => {
-                const data = snapshot.val() || {};
-                const fermentations = Object.values(data);
-                const lastTimeSpanInSec = this.getLastTimeSpanInSec(fermentations);
-                const lastBubbleAt = fermentations.length ? new Date(fermentations[0].at) : null; // fermentations is (already) reversed
+    firebase
+      .bubbles(batch)
+      .orderByKey()
+      //.limitToLast(20)
+      .on("value", snapshot => {
+        const data = snapshot.val() || {};
+        const bubbles = Object.values(data);
+        const lastTimeSpanInSec = this.getLastTimeSpanInSec(bubbles);
+        const lastBubbleAt = bubbles.length ? new Date(bubbles[0].at) : null; // bubbles is (already) reversed
 
-                this.setState({
-                    lastTimeSpanInSec: lastTimeSpanInSec,
-                    loading: false,
-                    lastBubbleAt: lastBubbleAt
-                });
-            });
-    }
-
-    getTimeSpanText(timeInSec) {
-        if (timeInSec < 60) {
-            return timeInSec + 's';
-        }
-        const hours = Math.floor(timeInSec / 3600);
-        const mins = Math.floor(timeInSec / 60);
-        const secs = timeInSec - (hours * 3600 + mins * 60); 
-        if (hours === 0) {
-            if (secs === 0) {
-                return mins + 'm';
-            }
-            return mins + ":" + secs + "m";
-        }
-        var date = new Date(null);
-        date.setSeconds(timeInSec);
-        return date.toLocaleTimeString();
-    }
-
-    componentDidMount() {
-        this.loadFermentations();
-        
-        let chart = am4core.create('' + this.id, am4charts.GaugeChart);
-        chart.innerRadius = am4core.percent(82);
-
-        const minValue = 0;
-        const maxValue = 60 * 10; 
-        
-        /**
-         * Normal axis
-         */
-
-        var axis = chart.xAxes.push(new am4charts.ValueAxis());
-        axis.min = minValue;
-        axis.max = maxValue;
-        axis.renderer.minGridDistance = 60;
-        axis.renderer.radius = am4core.percent(80);
-        axis.renderer.inside = true;
-        axis.renderer.line.strokeOpacity = 1;
-        axis.renderer.ticks.template.strokeOpacity = 1;
-        axis.renderer.ticks.template.length = 10;
-        axis.renderer.grid.template.disabled = true;
-        axis.renderer.labels.template.radius = 40;
-        axis.renderer.inversed = true;
-
-        const getTimeSpanText = this.getTimeSpanText;
-
-        axis.renderer.labels.template.adapter.add("text", function(text) {
-            if (!text) {
-                return text;
-            }
-            const val = text.replace(',', '');
-            return getTimeSpanText(val);
-        });
-
-        /**
-         * Axis for ranges
-         */
-
-        var colorSet = new am4core.ColorSet();
-
-        var axis2 = chart.xAxes.push(new am4charts.ValueAxis());
-        axis2.min = minValue;
-        axis2.max = maxValue;
-        axis2.renderer.minGridDistance = 60;
-        axis2.renderer.innerRadius = 10
-        axis2.strictMinMax = true;
-        axis2.renderer.labels.template.disabled = true;
-        axis2.renderer.ticks.template.disabled = true;
-        axis2.renderer.grid.template.disabled = true;
-        axis2.renderer.inversed = true;
-
-        var range0 = axis2.axisRanges.create();
-        range0.value = minValue;
-        range0.endValue = maxValue / 2;
-        range0.axisFill.fillOpacity = 1;
-        range0.axisFill.fill = colorSet.getIndex(0);
-
-        var range1 = axis2.axisRanges.create();
-        range1.value = maxValue / 2;
-        range1.endValue = maxValue;
-        range1.axisFill.fillOpacity = 1;
-        range1.axisFill.fill = colorSet.getIndex(2);
-
-        /**
-         * Label
-         */
-
-        var label = chart.radarContainer.createChild(am4core.Label);
-        label.isMeasured = false;
-        label.fontSize = 20;
-        label.x = am4core.percent(50);
-        label.y = am4core.percent(100);
-        label.horizontalCenter = "middle";
-        label.verticalCenter = "bottom";
-
-        /**
-         * Hand
-         */
-
-        var hand = chart.hands.push(new am4charts.ClockHand());
-        hand.axis = axis2;
-        hand.innerRadius = am4core.percent(20);
-        hand.startWidth = 10;
-        hand.pin.disabled = true;
-
-        hand.events.on("propertychanged", function(ev) {
-            const value = ev.target.value;
-            range0.endValue = value;
-            range1.value = value;
-            axis2.invalidate();
-        });
-
-        const { lastTimeSpanInSec } = this.state;
-        label.text = this.getTimeSpanText(lastTimeSpanInSec);
-        hand.value = Math.min(lastTimeSpanInSec, maxValue);
-        if (lastTimeSpanInSec > (60 * 60)) {
-            range0.axisFill.fill = am4core.color('red');
-        }
-
-        this.hand = hand;
-        this.label = label;
-        this.chart = chart;
-        this.range0 = range0;
-    }
-
-    componentWillUnmount() {
-        if (this.chart) {
-            this.chart.dispose();
-        }
-    }
-
-    componentDidUpdate(oldProps) {
-        const { lastTimeSpanInSec } = this.state;
-
-        this.label.text = this.getTimeSpanText(lastTimeSpanInSec);
-
-        if (lastTimeSpanInSec > (60 * 60)) {
-            this.range0.axisFill.fill = am4core.color('red');
-        }
-
-        var animation = new am4core.Animation(this.hand, {
-            property: "value",
-            to: Math.min(lastTimeSpanInSec, (10 * 60))
-        }, 1000, am4core.ease.cubicOut).start();
-    }
-
-    getLastTimeSpanInSec(fermentations) {
-        if (!fermentations) {
-            return 0;
-        }
-        const reversed = fermentations.reverse(); 
-        const minSpanInSec = 5; // avoid multiple bubbles for "one" bubble
-        let latestAt;
-        for (let [index, val] of reversed.entries()) {
-            if (latestAt) {
-                const previousAt = new Date(val.at);
-                const diffInSec = (latestAt - previousAt) / 1000;
-                if (diffInSec > minSpanInSec) {
-                    return Math.round(diffInSec);
-                }
-            } else {
-                latestAt = new Date(val.at);
-            }
-        }
-        return 0;
-    }
-
-    render() {
-        const { classes, batch } = this.props;
-        const { lastBubbleAt, loading } = this.state;
-        const now = new Date();
-        const lastBubbleIntervalInMs = lastBubbleAt ? (now - lastBubbleAt) : 0;
-        const longTimeAgo = lastBubbleIntervalInMs > (60 * 60 * 1000);
-
-        return (
-            <Card className={classes.card}>
-                <CardContent>
-                    <Typography className={classes.title} color="textSecondary" gutterBottom>
-                        Time between bubbles in batch {batch}
-                    </Typography>
-                    <div id={this.id} style={{  height: "200px" }}></div>
-                    <Typography component="p">
-                        {loading && 'Loading ...'}
-                        Last bubble at: { lastBubbleAt ? lastBubbleAt.toLocaleDateString() + ' ' + lastBubbleAt.toLocaleTimeString() : '-' }
-                        {longTimeAgo && <WarningIcon className={classes.icon} color="error"></WarningIcon>}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small" onClick={ this.doResetFermentationEntires }>Reset</Button>
-                </CardActions>
-            </Card>
-        );
-    }
-
-    doResetFermentationEntires() {
-        const { batch, firebase } = this.props;
-        
-        firebase.fermentations(batch).set({}); 
-        
         this.setState({
-            lastTimeSpanInSec: 0,
+          lastTimeSpanInSec: lastTimeSpanInSec,
+          loading: false,
+          lastBubbleAt: lastBubbleAt
         });
+      });
+  }
+
+  getTimeSpanText(timeInSec) {
+    if (timeInSec < 60) {
+      return timeInSec + "s";
     }
+    const hours = Math.floor(timeInSec / 3600);
+    const mins = Math.floor(timeInSec / 60);
+    const secs = timeInSec - (hours * 3600 + mins * 60);
+    if (hours === 0) {
+      if (secs === 0) {
+        return mins + "m";
+      }
+      return mins + ":" + secs + "m";
+    }
+    var date = new Date(null);
+    date.setSeconds(timeInSec);
+    return date.toLocaleTimeString();
+  }
+
+  componentDidMount() {
+    this.loadBubbles();
+
+    let chart = am4core.create("" + this.id, am4charts.GaugeChart);
+    chart.innerRadius = am4core.percent(82);
+
+    const minValue = 0;
+    const maxValue = 60 * 10;
+
+    /**
+     * Normal axis
+     */
+
+    var axis = chart.xAxes.push(new am4charts.ValueAxis());
+    axis.min = minValue;
+    axis.max = maxValue;
+    axis.renderer.minGridDistance = 60;
+    axis.renderer.radius = am4core.percent(80);
+    axis.renderer.inside = true;
+    axis.renderer.line.strokeOpacity = 1;
+    axis.renderer.ticks.template.strokeOpacity = 1;
+    axis.renderer.ticks.template.length = 10;
+    axis.renderer.grid.template.disabled = true;
+    axis.renderer.labels.template.radius = 40;
+    axis.renderer.inversed = true;
+
+    const getTimeSpanText = this.getTimeSpanText;
+
+    axis.renderer.labels.template.adapter.add("text", function(text) {
+      if (!text) {
+        return text;
+      }
+      const val = text.replace(",", "");
+      return getTimeSpanText(val);
+    });
+
+    /**
+     * Axis for ranges
+     */
+
+    var colorSet = new am4core.ColorSet();
+
+    var axis2 = chart.xAxes.push(new am4charts.ValueAxis());
+    axis2.min = minValue;
+    axis2.max = maxValue;
+    axis2.renderer.minGridDistance = 60;
+    axis2.renderer.innerRadius = 10;
+    axis2.strictMinMax = true;
+    axis2.renderer.labels.template.disabled = true;
+    axis2.renderer.ticks.template.disabled = true;
+    axis2.renderer.grid.template.disabled = true;
+    axis2.renderer.inversed = true;
+
+    var range0 = axis2.axisRanges.create();
+    range0.value = minValue;
+    range0.endValue = maxValue / 2;
+    range0.axisFill.fillOpacity = 1;
+    range0.axisFill.fill = colorSet.getIndex(0);
+
+    var range1 = axis2.axisRanges.create();
+    range1.value = maxValue / 2;
+    range1.endValue = maxValue;
+    range1.axisFill.fillOpacity = 1;
+    range1.axisFill.fill = colorSet.getIndex(2);
+
+    /**
+     * Label
+     */
+
+    var label = chart.radarContainer.createChild(am4core.Label);
+    label.isMeasured = false;
+    label.fontSize = 20;
+    label.x = am4core.percent(50);
+    label.y = am4core.percent(100);
+    label.horizontalCenter = "middle";
+    label.verticalCenter = "bottom";
+
+    /**
+     * Hand
+     */
+
+    var hand = chart.hands.push(new am4charts.ClockHand());
+    hand.axis = axis2;
+    hand.innerRadius = am4core.percent(20);
+    hand.startWidth = 10;
+    hand.pin.disabled = true;
+
+    hand.events.on("propertychanged", function(ev) {
+      const value = ev.target.value;
+      range0.endValue = value;
+      range1.value = value;
+      axis2.invalidate();
+    });
+
+    const { lastTimeSpanInSec } = this.state;
+    label.text = this.getTimeSpanText(lastTimeSpanInSec);
+    hand.value = Math.min(lastTimeSpanInSec, maxValue);
+    if (lastTimeSpanInSec > 60 * 60) {
+      range0.axisFill.fill = am4core.color("red");
+    }
+
+    this.hand = hand;
+    this.label = label;
+    this.chart = chart;
+    this.range0 = range0;
+  }
+
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
+  componentDidUpdate(oldProps) {
+    const { lastTimeSpanInSec } = this.state;
+
+    this.label.text = this.getTimeSpanText(lastTimeSpanInSec);
+
+    if (lastTimeSpanInSec > 60 * 60) {
+      this.range0.axisFill.fill = am4core.color("red");
+    }
+
+    var animation = new am4core.Animation(
+      this.hand,
+      {
+        property: "value",
+        to: Math.min(lastTimeSpanInSec, 10 * 60)
+      },
+      1000,
+      am4core.ease.cubicOut
+    ).start();
+  }
+
+  getLastTimeSpanInSec(bubbles) {
+    if (!bubbles || bubbles.length === 1) {
+      return 0;
+    }
+    const reversed = bubbles.reverse();
+    const lastBubbleEntry = reversed[0];
+    const lastBubbleAt = new Date(lastBubbleEntry.at);
+    const secondLastBubbleEntry = reversed[1];
+    const secondLastBubbleAt = new Date(secondLastBubbleEntry.at);
+    const timeSpanInSec = (lastBubbleAt - secondLastBubbleAt) / 1000;
+    const bubbleCount = lastBubbleEntry.count;
+    return Math.round(timeSpanInSec / bubbleCount); // will be the average for the time span
+  }
+
+  render() {
+    const { classes, batch } = this.props;
+    const { lastBubbleAt, loading } = this.state;
+    const now = new Date();
+    const lastBubbleIntervalInMs = lastBubbleAt ? now - lastBubbleAt : 0;
+    const longTimeAgo = lastBubbleIntervalInMs > 60 * 60 * 1000;
+
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Time between bubbles in batch {batch}
+          </Typography>
+          <div id={this.id} style={{ height: "200px" }} />
+          <Typography component="p">
+            {loading && "Loading ..."}
+            Last bubble at:{" "}
+            {lastBubbleAt
+              ? lastBubbleAt.toLocaleDateString() +
+                " " +
+                lastBubbleAt.toLocaleTimeString()
+              : "-"}
+            {longTimeAgo && (
+              <WarningIcon className={classes.icon} color="error" />
+            )}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={this.doResetBubbleEntires}>
+            Reset
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
+
+  doResetBubbleEntires() {
+    const { batch, firebase } = this.props;
+
+    firebase.bubbles(batch).set({});
+
+    this.setState({
+      lastTimeSpanInSec: 0
+    });
+  }
 }
 
 export default withStyles(styles)(withFirebase(Fermentation));
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,38 +1,37 @@
-import app from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 var config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 class Firebase {
-    constructor() {
-        app.initializeApp(config);
-        this.auth = app.auth();
-        this.db = app.database();
-    }
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+    this.db = app.database();
+  }
 
-    // *** Auth API ***
-    doCreateUserWithEmailAndPassword = (email, password) =>
-        this.auth.createUserWithEmailAndPassword(email, password);
-    doSignInWithEmailAndPassword = (email, password) =>
-        this.auth.signInWithEmailAndPassword(email, password);
-    doSignOut = () => this.auth.signOut();
-    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-    doPasswordUpdate = password =>
-      this.auth.currentUser.updatePassword(password);    
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 
-    // *** DB API ***
-    user = uid => this.db.ref(`users/${uid}`);
-    users = () => this.db.ref('users');      
-    fermentations = (batch) => this.db.ref('fermentations/' + batch);
-    devices = () => this.db.ref('device');
+  // *** DB API ***
+  user = uid => this.db.ref(`users/${uid}`);
+  users = () => this.db.ref("users");
+  bubbles = batch => this.db.ref("fermentation/bubbles/" + batch);
+  devices = () => this.db.ref("device");
 }
-  
+
 export default Firebase;
